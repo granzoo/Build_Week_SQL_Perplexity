6ce7740ecce5ffa05bad359b25412da8f03085ce
Alberto Granzo <agranzo98@gmail.com>
2026-04-14 20:55:24 +0200

    Update Query vendite.sql

--- Query vendite.sql
+++ Query vendite.sql
@@ -3,17 +3,16 @@
 START TRANSACTION;
 
 INSERT INTO vendite (id_vendita, data_transazione, id_negozio)
-VALUES (15, CURDATE(), 1);
+  VALUES   (15, CURDATE(), 1);
 
 INSERT INTO dettaglio_vendita (id_vendita, id_prodotto, quantita, unit_price)
-VALUES (15, 6, 3, 699.99);
+  VALUES   (15, 6, 3, 699.99);
 
 UPDATE giacenze g
-JOIN negozi n ON g.id_magazzino = n.id_magazzino
-SET g.quantita_disponibile = g.quantita_disponibile - 3
+  JOIN negozi n
+  ON g.id_magazzino = n.id_magazzino
+  SET g.quantita_disponibile = g.quantita_disponibile - 3
 WHERE n.id_negozio = 1
   AND g.id_prodotto = 6;
 
 COMMIT;
-
-select * from giacenze;
