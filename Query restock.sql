dc8f5873d833d34c470aa3e21c0405ffc758f838
Alberto Granzo <agranzo98@gmail.com>
2026-04-16 16:48:50 +0200

    Update Query restock.sql
    added comments

--- Query restock.sql
+++ Query restock.sql
@@ -1,11 +1,18 @@
+/*
+Query per il restock:
+Qui non c'è nessu input manuale da dare, semplicemente usiamo la DML per controllare la
+merce in magazzino che è sotto la soglia della propria categoria, in modo da facilitare
+il processo di acquisto agli addetti al restock.
+*/
+
 SELECT 
-    g.id_magazzino,
-    m.nome_magazzino,
-    g.id_prodotto,
-    p.nome_prodotto,
-    g.quantita_disponibile,
-    s.livello_restock,
-    IF(s.livello_restock < g.quantita_disponibile, 'IN STOCK', 'FARE RESTOCK') AS giacenza_magazzino
+    g.id_magazzino,				-- 	\
+    m.nome_magazzino,			-- 	-\
+    g.id_prodotto,				-- 	--> informazioni di base
+    p.nome_prodotto,			--  -/
+    g.quantita_disponibile,		-- -/
+    s.livello_restock,			-- /
+    IF(s.livello_restock < g.quantita_disponibile, 'IN STOCK', 'FARE RESTOCK') AS giacenza_magazzino	-- "suddivide" la merce sufficientemente stoccata da quella che necessita restock, restituendo un valore "binario"
 FROM giacenze g
 JOIN magazzini m 
     ON g.id_magazzino = m.id_magazzino
@@ -13,9 +20,5 @@ JOIN anagrafica_prodotto p
     ON g.id_prodotto = p.id_prodotto
 JOIN soglie_restock s
 	ON g.id_magazzino = s.id_magazzino
-GROUP BY g.id_prodotto, g.id_magazzino, s.livello_restock;
-
-
-
-
-
+GROUP BY g.id_prodotto, g.id_magazzino, s.livello_restock	-- l'addetto al restock visualizza solamente la merce che necessita la sua attenzione per procedere con l'operazione di acquisto dai fornitori
+HAVING giacenza_magazzino = 'FARE RESTOCK';
