0c4c376f168eae52398a50a74f3258cb1de6d959
Alberto Granzo <agranzo98@gmail.com>
2026-04-16 16:54:25 +0200

    Update Query vendite.sql
    added comments and fixed "INSERT INTO dettaglio_vendita (..."

--- Query vendite.sql
+++ Query vendite.sql
@@ -1,18 +1,36 @@
--- vendite
+/*
+Query per le vendite:
+L'operatore di vendita segue il processo di vendita dei singoli prodotti in ogni negozio,
+aggiornando immediatamente la giacenza disponibile in magazzino (supponendo che il refill
+della merce venduta, cioè il passaggio della merce da magazzino a negozio avvenga
+in maniera istantanea). Il triplo "SELECT" a fine query funge da ulteriore verifica della
+corretta transazione, restituendo ad ogni fine transazione la nuova vendita inserita
+e la nuova giacenza aggiornata.
+*/
 
 START TRANSACTION;
 
 INSERT INTO vendite (id_vendita, data_transazione, id_negozio)
-  VALUES   (15, CURDATE(), 1);
+VALUES (17, CURDATE(), 1);								                     	-- supponendo che ogni negozio (id_negozio) abbia un addetto all'inserimento di ordini eseguiri, l'unico input manuale qui è id_vendita.
 
 INSERT INTO dettaglio_vendita (id_vendita, id_prodotto, quantita, unit_price)
-  VALUES   (15, 6, 3, 699.99);
+SELECT
+	(SELECT MAX(id_vendita) FROM vendite),
+    p.id_prodotto,
+    3,															                            -- input manuale
+    p.prezzo_unitario
+FROM anagrafica_prodotto p
+WHERE p.id_prodotto = 6;										                    -- input manuale
 
 UPDATE giacenze g
-  JOIN negozi n
-  ON g.id_magazzino = n.id_magazzino
-  SET g.quantita_disponibile = g.quantita_disponibile - 3
-WHERE n.id_negozio = 1
-  AND g.id_prodotto = 6;
+JOIN negozi n ON g.id_magazzino = n.id_magazzino
+JOIN dettaglio_vendita dv ON dv.id_prodotto = g.id_prodotto
+SET g.quantita_disponibile = g.quantita_disponibile - dv.quantita
+WHERE n.id_negozio = 1											                      -- numero fisso del negozio, verrà fornita ad ogni negozio una query con il codice corrispondente al negozio
+  AND dv.id_vendita = (SELECT MAX(id_vendita) FROM vendite);	    -- nessun input manuale
 
 COMMIT;
+
+SELECT * FROM giacenze;
+SELECT * FROM dettaglio_vendita;
+SELECT * FROM vendite;
